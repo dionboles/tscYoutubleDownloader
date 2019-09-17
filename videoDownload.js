21539a51447694d85e1e19491cc2de20b8bf78ff
Dion Boles <dion.boles@gmail.com>
2019-09-17 11:58:31 -0400

    Delete videoDownload.js

--- videoDownload.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var getVideo_1 = require("./getVideo");
-var a = new getVideo_1["default"]("https://www.youtube.com/watch?v=OZHinPswQ1g&t=253s", "test.mp4");
-a.startDownload();
